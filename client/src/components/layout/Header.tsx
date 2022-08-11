17841fd12e5fbecc616bdf89851cbc825ab2ab8a
carlosrrdev <rr.carlos.dev@gmail.com>
2022-08-11 10:34:21 -0500

    Add prettier tailwind plugin

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -3,8 +3,8 @@ import ThemeToggle from "../ThemeToggle";
 
 const Header: React.FC = () => {
   return (
-    <header className="p-6 w-full max-w-screen-xl mx-auto">
-      <nav className="flex justify-between items-center">
+    <header className="mx-auto w-full max-w-screen-xl p-6">
+      <nav className="flex items-center justify-between">
         <p className="text-2xl font-black">
           Voting With <br />
           Friends
