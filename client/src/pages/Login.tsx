7b5f30bd5e8df3e7ff6bf9aa2fe0df8e198ee07c
samschool789@gmail.com <samschool789@gmail.com>
2022-08-14 12:07:40 -0500

    starter code

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import MotionWrapper from "../components/layout/MotionWrapper";
 
@@ -5,7 +6,27 @@ const Login: React.FC = () => {
   return (
     <Layout>
       <MotionWrapper>
-        <h1>Login Page</h1>
+        <h1 className="mx-auto mb-4 text-center text-2xl">
+          Welcome to Voting with Friends!!!
+        </h1>
+        <div className="form">
+          <form>
+            <div className="input-container">
+              <label className="text-2xl">Email</label>
+              <input type="text" name="uname" required />
+            </div>
+            <div className="input-container">
+              <label className="padding: 2px text-2xl">Password </label>
+              <input type="password" name="pass" required />
+            </div>
+            <Link
+              className="mx-auto mb-4 inline-block w-1/2 rounded bg-indigo-500 py-4 text-center text-white md:mx-0 md:mr-auto lg:mb-0"
+              to="/polls"
+            >
+              Submit
+            </Link>
+          </form>
+        </div>
       </MotionWrapper>
     </Layout>
   );
