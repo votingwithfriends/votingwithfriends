8e1c19a4189c5986a12ae2f13906d2e2512e3b35
Leonardo <josue.aguilar1995@gmail.com>
2022-08-22 18:41:48 -0500

    added poll queries to client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -25,7 +25,82 @@ export const QUERY_ME = gql`
   }
 `;
 
-// export const QUERY_USERS = gql`
-//   query users {
-//   }
-// `;
+export const QUERY_USER = gql`
+  query User($id: ID!) {
+    user(_id: $id) {
+      _id
+      username
+      email
+      polls {
+        _id
+        title
+        is_open
+      }
+      friends {
+        _id
+        username
+      }
+      friendCount
+    }
+  }
+`;
+
+export const QUERY_POLLS = gql`
+  query allPolls {
+    polls {
+      _id
+      title
+      is_open
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_POLL = gql`
+  query Poll($id: ID!) {
+    poll(_id: $id) {
+      _id
+      title
+      is_open
+      choices {
+        _id
+        choice_name
+      }
+      votes {
+        _id
+        rank_value
+        user_id
+        choice_id
+      }
+      comments {
+        _id
+        comment_body
+        username
+        createdAt
+      }
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_POLL_VOTES = gql`
+  query Poll($id: ID!) {
+    poll(_id: $id) {
+      _id
+      title
+      is_open
+      votes {
+        _id
+        rank_value
+        user_id
+        choice_id
+      }
+    }
+  }
+`;
