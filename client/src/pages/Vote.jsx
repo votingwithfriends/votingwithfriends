5b631fb21bfb2fb5f2b8371da6e6696b4f36ffa1
Leonardo <josue.aguilar1995@gmail.com>
2022-08-23 21:57:52 -0500

    adjusted vote page

--- client/src/pages/Vote.jsx
+++ client/src/pages/Vote.jsx
@@ -1,46 +1,64 @@
 import Layout from "../components/layout/Layout";
 import MotionWrapper from "../components/layout/MotionWrapper";
+import { QUERY_POLL } from "../utils/queries";
+import { useQuery } from "@apollo/client";
 
 const Vote = () => {
+  const { loading, error, data } = useQuery(QUERY_POLL, {
+    variables: { id: "63040aba1e97eddde0783bad" },
+  });
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  const pollChoices = data.poll.choices;
+
   return (
     <Layout>
       <MotionWrapper>
         <article>
-          <p class="mb-4 font-bold md:text-2xl">
+          {console.log(pollChoices)}
+          <p className="mb-4 font-bold md:text-2xl">
             Please select your top three options below, leaving the rest blank.
           </p>
-          <h3 class="mb-8 text-xl font-bold text-blue-500 md:text-2xl"></h3>
+          <h3 className="mb-8 text-xl font-bold text-blue-500 md:text-2xl">
+            {data.poll.title}
+          </h3>
           <form id="vote-form">
-            <div class="flex items-center justify-between">
+            <div className="flex items-center justify-between">
               <ul
                 id="choice-list"
-                class="flex flex-col gap-y-4"
+                className="flex flex-col gap-y-4"
                 data-poll_id="{{id}}"
               >
-                <li
-                  class="flex items-center justify-between rounded px-4 py-2"
-                  data-choice="{{choice_name}}"
-                  data-choice_id="{{id}}"
-                >
-                  <label for="choice" class="font-bold"></label>
-                  <select name="choice" class="ml-6 p-2">
-                    <option value="0">None</option>
-                    <option class="text-green-500" value="6">
-                      1st Place
-                    </option>
-                    <option class="text-yellow-500" value="3">
-                      2nd Place
-                    </option>
-                    <option class="text-red-500" value="2">
-                      3rd Place
-                    </option>
-                  </select>
-                </li>
+                {pollChoices.map(({ _id, choice_name, __typename }) => (
+                  <li
+                    className="flex items-center justify-between rounded px-4 py-2"
+                    data-choice={choice_name}
+                    key={_id}
+                  >
+                    <label htmlFor="choice" className="font-bold">
+                      {choice_name}
+                    </label>
+                    <select name="choice" className="ml-6 p-2 text-gray-700">
+                      <option value="0">None</option>
+                      <option className="text-green-500" value="6">
+                        1st Place
+                      </option>
+                      <option className="text-yellow-500" value="3">
+                        2nd Place
+                      </option>
+                      <option className="text-red-500" value="2">
+                        3rd Place
+                      </option>
+                    </select>
+                  </li>
+                ))}
               </ul>
             </div>
             <button
               type="submit"
-              class="mt-8 rounded bg-green-500 p-4 text-white hover:bg-green-400"
+              className="mt-8 rounded bg-green-500 p-4 text-white hover:bg-green-400"
             >
               Submit Votes
             </button>
