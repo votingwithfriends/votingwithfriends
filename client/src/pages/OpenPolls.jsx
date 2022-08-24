09b5a386619260a0553254e322c4de85ba478970
carlosrrdev <rr.carlos.dev@gmail.com>
2022-08-24 14:24:54 -0500

    adjustments

--- client/src/pages/OpenPolls.jsx
+++ client/src/pages/OpenPolls.jsx
@@ -1,6 +1,7 @@
 import MotionWrapper from "../components/layout/MotionWrapper";
 import Layout from "../components/layout/Layout";
 import { QUERY_POLLS } from "../utils/queries";
+import { IoPerson } from "react-icons/io5";
 import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
@@ -15,12 +16,12 @@ const OpenPolls = () => {
   return (
     <Layout>
       <MotionWrapper>
-        <article className="mx-auto w-full max-w-screen-md">
+        <article className="mx-auto mt-20 w-full max-w-screen-md">
           <h2 className="mb-8 text-xl font-bold md:text-3xl">Polls</h2>
           <ul className="flex flex-col gap-y-4">
             {pollArr.map(({ _id, title, is_open, user, ...rest }) => (
               <li
-                className="rounded border border-green-200 transition-all ease-in-out hover:border-green-500"
+                className="rounded bg-blue-100 transition-all ease-in-out dark:bg-zinc-700"
                 key={_id}
               >
                 <Link to={`/vote/${_id}`}>
@@ -28,11 +29,14 @@ const OpenPolls = () => {
                     <div className="flex items-center justify-between">
                       <div>
                         <p className="text-lg font-bold">{title}</p>
-                        <p className="text-gray-600">{user.username}</p>
+                        <p className="opacity-70">
+                          <IoPerson className="mr-1 inline" />
+                          {user.username}
+                        </p>
                       </div>
                       {is_open && (
-                        <p className="rounded bg-green-500 py-2 px-4 text-white">
-                          Poll Open
+                        <p className="rounded bg-blue-500 py-2 px-4 text-white dark:bg-cyan-600">
+                          Active
                         </p>
                       )}
                     </div>
